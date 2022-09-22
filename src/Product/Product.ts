637f36694f3cb4b9849139007302f73a8842bf1d
diskszk <ibalucky23@gmail.com>
2022-09-22 22:26:34 +0900

    fix: Productクラスに初期化時のバリデーションを作成

--- src/Product/Product.ts
+++ src/Product/Product.ts
@@ -1,3 +1,7 @@
 export class Product {
-  constructor(readonly name: string, readonly value: number) {}
+  constructor(readonly name: string, readonly value: number) {
+    if (!name || value < 0) {
+      throw new Error("不正な製品が投入されました。");
+    }
+  }
 }
